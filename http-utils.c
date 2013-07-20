5adb9633beb8e854a6e10aa2dc30236ab1f963a1
Matthew Tole <pebble@matthewtole.com>
2013-07-20 17:09:46 +0100

    Add basic httpebble error function.

---
+++ http-utils.c
@@ -0,0 +1,79 @@
+include "http-utils.h"
+
+char* httpebble_message(int error_code) {
+  if (error_code > 1000) {
+    error_code -= 1000;
+  }
+
+  switch (error_code) {
+    case HTTP_SEND_TIMEOUT:
+      return "SEND TIMEOUT";
+    break;
+    case HTTP_SEND_REJECTED:
+      return "SEND REJECTED";
+    break;
+    case HTTP_NOT_CONNECTED:
+      return "NOT CONNECTED";
+    break;
+    case HTTP_BRIDGE_NOT_RUNNING:
+      return "BRIDGE NOT RUNNING";
+    break;
+    case HTTP_INVALID_ARGS:
+      return "INVALID ARGS";
+    break;
+    case HTTP_BUSY:
+      return "BUSY";
+    break;
+    case HTTP_BUFFER_OVERFLOW:
+      return "BUFFER OVERFLOW";
+    break;
+    case HTTP_ALREADY_RELEASED:
+      return "ALREADY RELEASED";
+    break;
+    case HTTP_CALLBACK_ALREADY_REGISTERED:
+      return "CALLBACK ALREADY REGISTERED";
+    break;
+    case HTTP_CALLBACK_NOT_REGISTERED:
+      return "CALLBACK NOT REGISTERED";
+    break;
+    case HTTP_NOT_ENOUGH_STORAGE:
+      return "NOT ENOUGH STORAGE";
+    break;
+    case HTTP_INVALID_DICT_ARGS:
+      return "INVALID DICT ARGS";
+    break;
+    case HTTP_INTERNAL_INCONSISTENCY:
+      return "INTERNAL INCONSISTENCY";
+    break;
+    case HTTP_INVALID_BRIDGE_RESPONSE:
+      return "INVALID BRIDGE RESPONSE";
+    break;
+    case 400:
+      return "Bad Request";
+    break;
+    case 401:
+      return "Unauthorized";
+    break;
+    case 403:
+      return "Forbidden";
+    break;
+    case 404:
+      return "Not Found";
+    break;
+    case 405:
+      return "Method Not Allowed"
+    break;
+  }
+
+  if (error >= 500 && error < 600) {
+    return "HTTP Server Error";
+  }
+  else if (error > 400 && error < 500) {
+    return "HTTP Client Error";
+  }
+  else if (error > 300 && error < 300) {
+    return "HTTP Redirection";
+  }
+
+  return "Unknown Error";
+}
---
+++ http-utils.h
@@ -0,0 +1,6 @@
+#ifndef HTTP_UTILS_H
+#define HTTP_UTILS_H
+
+char* httpebble_message(int error_code);
+
+#endif // HTTP_UTILS_H
